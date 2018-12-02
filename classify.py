d557cc9a5f49cab6727006862eed9d9dba8de5d8
AlexJerome <alexandra.jerome@student.ecp.fr>
2018-12-02 16:24:29 +0100

    changes classify

--- classify.py
+++ classify.py
@@ -5,7 +5,7 @@ def classify(trainSet, trainLabels, testSet):
     predictedLabels = zeros(testSet.shape[0])
 
     for i in range(testSet.shape[0]):
-        if testSet[i, 3] == 'female':
+        if testSet[i, 4] == 'female':
             predictedLabels[i] = 1
 
     return predictedLabels
